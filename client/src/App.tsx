c65bb62f46f1fdeea251af1ec0f9cc2047ecaa53
Vladan Velojic <v.velojic@vegaitsourcing.rs>
2019-06-05 20:44:03 +0200

    freaking hell with typescript and prettier for the first time fetching data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
+import * as React from 'react';
 import './App.css';
 
+interface User {
+    _id: number;
+    name: string;
+    password: string;
+    email: string;
+}
 interface AppState {
-  count: number;
+    users: User[];
 }
 
 class App extends React.Component<{}, AppState> {
+    public state: AppState = {
+        users: [],
+    };
 
-  public state: AppState = {
-    count: 0,
-  };
+    public componentDidMount(): void {
+        fetch('http://localhost:3001/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users: users.users }));
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            sadasdas
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    public render(): React.ReactNode {
+        console.log(this.state.users);
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        {this.state.users.map(
+                            (user): JSX.Element => (
+                                <li key={user._id}>
+                                    index: {user._id} {user.name}
+                                </li>
+                            ),
+                        )}
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
