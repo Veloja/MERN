baf1965d0f7bf5b6804b7cf0a2acdd48c463f24d
Vladan Velojic <v.velojic@vegaitsourcing.rs>
2019-06-10 15:02:41 +0200

    user registerin for the first time is sent to db with hashed bassword

--- api/models/user.js
+++ api/models/user.js
@@ -11,4 +11,4 @@ const userSchema = new Schema({
 })
 
 // Model
-module.exports = mongoose.model('User', userSchema);;
+module.exports = mongoose.model('User', userSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
+
+router.post('/', (req, res, next) => {
+	User.find({email: req.body.email})
+	.exec()
+	.then(user => {
+		if(user.length >= 1) {
+			return res.status(409).json({
+				message: 'User with this email already exist'
+			})
+		} else {
+			bcrypt.hash(req.body.password, 10, (err, hash) => {
+				if(err) {
+					return res.status(500).json({
+						error: err
+					})
+				} else {
+					const newUser = new User({
+						_id: new mongoose.Types.ObjectId(),
+						name: req.body.name,
+						email: req.body.email,
+						password: hash
+					})
+					newUser
+					.save()
+					.then(savedUser => {
+						res.status(201).json({
+							message: 'Created product sucessfully',
+							createdUser: {
+								_id: res._id,
+								name: res.name,
+								requset: {
+									type: 'GET',
+									url: 'http://localhost:3001/users/' + res._id
+								}
+							}
+						})
+					})
+				}
+			})
+		}
+	})
+	.catch(err => {
+		res.status(400).json({
+			message: 'Some error occured',
+			err: err
+		})
+	});
+});
 
 router.get('/', (req, res, next) => {
 	User.find()
@@ -33,48 +82,6 @@ router.get('/', (req, res, next) => {
 		})
 	})
 });
-
-router.post('/', (req, res, next) => {
-	const newUser = new User({
-		_id: new mongoose.Types.ObjectId(),
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password
-	})
-	User.find({email: req.body.email})
-	.exec()
-	.then(user => {
-		if(user.length >= 1) {
-			return res.status(409).json({
-				message: 'User with this email already exists',
-				user: user
-			})
-		} else {
-			newUser
-			.save()
-			.then(savedUser => {
-				res.status(201).json({
-					message: 'Created product sucessfully',
-					createdUser: {
-						_id: res._id,
-						name: res.name,
-						requset: {
-							type: 'GET',
-							url: 'http://localhost:3001/users/' + res._id
-						}
-					}
-				})
-			})
-		}
-	})
-	.catch(err => {
-		res.status(400).json({
-			message: 'Some error occured',
-			err: err
-		})
-	});
-});
-
 
 router.get('/:userId', (req, res, next) => {
 	const id = req.params.userId;
